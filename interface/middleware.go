204ca02db0af7b0828962d7165c967fb6c8c83de
devfajar <fajartrengginas@gmail.com>
2022-11-23 22:28:21 +0700

    learn basic web golang

---
+++ interface/middleware.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+)
+
+const USERNAME = "batman"
+const PASSWORD = "secret"
+
+func MiddlewareAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			w.Write([]byte(`something went wrong`))
+			return
+		}
+
+		isValid := (username == USERNAME) && (password == PASSWORD)
+		if !isValid {
+			w.Write([]byte(`wrong username/password`))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func MiddlewareAllowOnlyGET(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.Write([]byte(`Only GET is Allowed`))
+			return
+	}
+
+	next.ServeHTTP(w, r)
+	})
+}
